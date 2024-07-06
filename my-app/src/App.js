f849c70e771e1e2d71ca65797e81da7d4baf3a74
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-07-06 17:49:42 +0300

    add form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,71 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 
+const initialState = {
+	email: '',
+	login: '',
+	password: '',
+};
+
+const useStore = () => {
+	const [state, setState] = useState(initialState);
+
+	return {
+		getState: () => state,
+		updateState: (fieldName, newValue) => {
+			setState({ ...state, [fieldName]: newValue });
+		},
+		resetState: () => {
+			setState(initialState);
+		},
+	};
+};
+
+const sendFormData = (formData) => {
+	console.log(formData);
+};
+
 export const App = () => {
+	const { getState, updateState, resetState } = useStore();
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		sendFormData(getState());
+	};
+
+	const { email, login, password } = getState();
+
+	const onChange = ({ target }) => updateState(target.name, target.value);
+
 	return (
 		<div className={styles.app}>
-			<input type="checkbox" />
-			<div>Любой контент</div>
+			<form onSubmit={onSubmit}>
+				<input
+					name="email"
+					type="email"
+					placeholder="Почта"
+					value={email}
+					onChange={onChange}
+				/>
+				<input
+					name="login"
+					type="text"
+					placeholder="Логин"
+					value={login}
+					onChange={onChange}
+				/>
+				<input
+					name="password"
+					type="password"
+					placeholder="Пароль"
+					value={password}
+					onChange={onChange}
+				/>
+				<button type="button" onClick={resetState}>
+					Сброc
+				</button>
+				<button type="submit">Отправить</button>
+			</form>
 		</div>
 	);
 };
